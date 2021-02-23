2647094fc81699916052466857a9fe149ecaa9e6
unknown <kalan.jiva@gmail.com>
2021-02-23 18:55:56 -0500

    'feb23-2'

---
+++ test_run.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 17:21:07 2021
+
+@author: kalan
+"""
+
+import ufcstatsScraper
+
+ufcstatsScraper.get_all_fights()
--- ufcstatsScraper.py
+++ ufcstatsScraper.py
@@ -4,19 +4,18 @@ import string
 import pandas as pd
 import time as t  
 import json
+#import sqlite3
 
-
+##############################################################################
 def get_all_fighter_pages():
     alphabet = string.ascii_lowercase
- 
+
     urls = []
     for letter in alphabet:
         url ="http://ufcstats.com/statistics/fighters?char=%s&page=all" %letter
-        urls.append(url)
-    
- 
-    
+        urls.append(url)  
     all_fighters={}
+    
     for url in urls:
         r = requests.get(url)
         soup = bs.BeautifulSoup(r.content,'lxml')
@@ -40,89 +39,42 @@ def get_all_fighter_pages():
     #     all_fighters[i] = ' '.join(all_fighters[i])
         
     return all_fighters
-
-start = t.time()
-all_fighters = get_all_fighter_pages()
-#need to run get_all_fighters infrequently, only when new fighters are signed and have a fight in UFC
-#how often does that happen...can probably schedule it once a month 
-stop = t.time()
-time= stop-start 
-print('This took %s seconds' %time)   #19 seconds for 3597 fighters
-
-with open('ufcfighters.json','w') as fp:
-    json.dump(all_fighters,fp)
+##############################################################################
+def get_all_fights():
     
-
-##########################
-
-with open('ufcfighters.json','r') as fp:
-    all_fighters = json.load(fp)
-
-def get_all_fights(fighters):
+    with open('ufcfighters.json','r') as fp:
+        all_fighters = json.load(fp)
     
+    fighters =list(all_fighters.keys())[0:100]
+    fighter_count=0
     ufcfightdata={}
+    
+    start = t.time()
+    
     for fighter in fighters:
         ufcfightdata[fighter[36:52]]={}
         r = requests.get(fighter)
         soup = bs.BeautifulSoup(r.content,'lxml')
-        
+        fighter_count +=1
         cnt=0
         for a in soup.find_all('a', href=True):
             if "fight-details" in a['href']:
                 if a['href'] not in ufcfightdata and a.text !='next' and 'Matchup' not in a.text:
                     ufcfightdata[fighter[36:52]][cnt] = {}
-                    ufcfightdata[fighter[36:52]][cnt][a['href']] = pd.read_html(a['href'])
-                    cnt+=1                        
-                                                   
-    return ufcfightdata
-          
-
-uniq_afighters=list(all_fighters.keys())
-fighters =list(uniq_afighters)[0:2]
-
-start = t.time()
-ufcfightdata = get_all_fights(fighters)                
-stop = t.time()     
-time= stop-start           
-print('This took %s seconds' %time)
-
-
-##########################        
-
-ufcfightdata={}
-for fighter in fighters:
-    ufcfightdata[fighter[36:52]]={}
-    r = requests.get(fighter)
-    soup = bs.BeautifulSoup(r.content,'lxml')
-    
-    cnt=0
-    for a in soup.find_all('a', href=True):
-        if "fight-details" in a['href']:
-            if a['href'] not in ufcfightdata:
-                print(a['href'])
-                ufcfightdata[fighter[36:52]][cnt] = {}
-                try:
-                    ufcfightdata[fighter[36:52]][cnt][a['href']] = pd.read_html(a['href'])
-                except ValueError:
-                    pass
-                cnt+=1      
-                
-#TO DO
-
-#2 def update() --delta update - get ready to test for JairzinhoRozenstruik CirylGane
-    #Ciryl Gane -787bb1f087ccff8a
-    #Rozenstriuk-2cd428e9606856fd
-    ## next match id 4a704dae3091adaf
-    ## match preview id 4a704dae3091adaf
-    ##what does the fight ID become for this fight? 
-  #2h
-  
-#3 read through 4 pandas DFs per fighter in the right manner and insert to table in sqlDB 
-    #test for Ferguson - read to db table - stats by event insert 
-    #"http://ufcstats.com/fighter-details/22a92d7f62195791"
-  #2h
-  
-#4 full run
-    #run on GCP rather than local machine for 12 hours? 
-    #1hr
+                    try:
+                        ufcfightdata[fighter[36:52]][cnt][a['href']] = pd.read_html(a['href'])
+                    except ValueError:
+                        pass
+                    cnt+=1   
+                     
+        if fighter_count%5 ==0:
+            print(fighter_count,'fighters have been processed')
         
+    
+    stop = t.time()   
+    time= stop-start 
+    print('This took %s seconds' %time)  
+                                     
+    return ufcfightdata     
+
+
